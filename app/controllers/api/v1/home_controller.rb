aac6aac42ddf25b7f2e1cbc656bd1f4fefb7ee20
Ana Karina Caetano <>
2025-05-29 10:42:30 -0300

    feat: correção das rotas

---
+++ app/controllers/api/v1/home_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  module V1
+    class HomeController < ApplicationController
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,49 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
 
-  defaults format: :json do
-    resources :evaluations, only: [:index, :show, :create] do
-      member do
-        post :submit_answers     # POST /evaluations/:id/submit_answers
-        get  :results            # GET  /evaluations/:id/results
-      end
+      defaults format: :json do
+        resources :evaluations, only: [:index, :show, :create] do
+          member do
+            post :submit_answers     # POST /evaluations/:id/submit_answers
+            get  :results            # GET  /evaluations/:id/results
+          end
 
-      collection do
-        post :start_objective    # POST /evaluations/start_objective
-        post :start_written      # POST /evaluations/start_written
-        get  :my                 # GET  /evaluations/my
+          collection do
+            post :start_objective    # POST /evaluations/start_objective
+            post :start_written      # POST /evaluations/start_written
+            get  :my                 # GET  /evaluations/my
+          end
+        end
+
+
+        resources :law_areas, only: [:index, :show] do
+          member do
+            get :questions          # GET /law_areas/:id/questions (questões por área)
+            get :performance        # GET /law_areas/:id/performance (desempenho por área)
+          end
+        end
+
+        resources :questions, only: [:index, :show] do
+          collection do
+            get :random             # GET /questions/random
+          end
+        end
+
+        resources :users, only: [:index, :show, :update] do
+          member do
+            get :performance        # GET /users/:id/performance
+          end
+        end
+
+        # Respostas do Usuário
+        resources :user_answers, only: [:create, :update]
+
+        # Desempenho do Usuário (geral)
+        resources :user_performances, only: [:index, :show]
+
       end
     end
-
-
-    resources :law_areas, only: [:index, :show] do
-      member do
-        get :questions          # GET /law_areas/:id/questions (questões por área)
-        get :performance        # GET /law_areas/:id/performance (desempenho por área)
-      end
-    end
-
-    resources :questions, only: [:index, :show] do
-      collection do
-        get :random             # GET /questions/random
-      end
-    end
-
-    resources :users, only: [:index, :show, :update] do
-      member do
-        get :performance        # GET /users/:id/performance
-      end
-    end
-
-    # Respostas do Usuário
-    resources :user_answers, only: [:create, :update]
-
-    # Desempenho do Usuário (geral)
-    resources :user_performances, only: [:index, :show]
-
   end
 end
